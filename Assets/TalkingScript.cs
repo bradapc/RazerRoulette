4cc3252931a0c435b57b0d47ef42298209471c2a
RKovl <ryan.kovlaske@gmail.com>
2024-10-27 10:46:32 -0700

    removed unused assetss, added text

---
+++ Assets/TalkingScript.cs
@@ -0,0 +1,68 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System.Collections;
+
+public class TalkingScript : MonoBehaviour
+{
+
+    public TextMeshProUGUI speakingText;
+
+    float countdownTime = 3f;
+    float countdownTimeTwo = 0.5f;
+    bool timeStart = false;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        showText();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeStart)
+        {   
+
+            StartCoroutine(fadeInText());
+            countdownTime -= Time.deltaTime;
+            if(countdownTime <= 0){
+                speakingText.gameObject.SetActive(false);
+                StartCoroutine(fadeOutText());
+                countdownTimeTwo -= Time.deltaTime;
+                if(countdownTimeTwo <= 0){
+                    countdownTime = 3f;
+                    countdownTimeTwo = 0.5f;
+                    timeStart = false;
+                    speakingText.gameObject.SetActive(false);
+                }
+                
+            }
+            
+        }
+    }
+
+    public IEnumerator fadeInText(){
+        speakingText.color = new Color(speakingText.color.r, speakingText.color.g, speakingText.color.b, 0);
+        while (speakingText.color.a < 1.0f)
+        {
+            speakingText.color = new Color(speakingText.color.r, speakingText.color.g, speakingText.color.b, speakingText.color.a + (Time.deltaTime / 1));
+            yield return null;
+        }
+    }
+    public IEnumerator fadeOutText(){
+        speakingText.color = new Color(speakingText.color.r, speakingText.color.g, speakingText.color.b, 1);
+        while (speakingText.color.a > 0.0f)
+        {
+            speakingText.color = new Color(speakingText.color.r, speakingText.color.g, speakingText.color.b, speakingText.color.a - (Time.deltaTime / 1));
+            yield return null;
+        }
+    }
+    public void showText(){
+        timeStart = true;
+    }
+
+    public void changeText(){
+
+    }
+}
